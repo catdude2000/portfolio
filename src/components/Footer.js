17234242d90f3b94cd838aa02c3398a673246c1d
Michael Pace <michael.g.pace@gmail.com>
2022-07-29 16:48:19 -0500

    added backimage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,7 +31,7 @@ class Footer extends Component {
               >
                 <span
                   className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
+                  // data-icon="twemoji-flag-for-flag-united-kingdom"
                   data-inline="false"
                   id={window.$primaryLanguageIconId}
                 ></span>
@@ -46,7 +46,7 @@ class Footer extends Component {
                 style={{ display: "inline" }}
               >
                 <span
-                  className="iconify language-icon"
+                  // className="iconify language-icon"
                   data-icon="twemoji-flag-for-flag-poland"
                   data-inline="false"
                   id={window.$secondaryLanguageIconId}
